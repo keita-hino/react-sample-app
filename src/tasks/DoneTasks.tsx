b56f71e79e9c7a568ab79dff5aa5c1e1ab5b29a5
keita <keitahino0415@gmail.com>
2021-04-10 11:43:17 +0900

    完了タスクを表示

--- src/tasks/DoneTasks.tsx
+++ src/tasks/DoneTasks.tsx
@@ -1,10 +1,30 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import { db } from '../lib/firebase'
+import { Task } from '../types/task'
+
+const useDoneTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const getTasks = async () => {
+    const collection = await db.collection('tasks').get();
+    const fetchTasks = collection.docs.map(doc => doc.data()) as Task[];
+    setTasks(fetchTasks.filter(task => task.status === 'done'));
+  }
+
+  return { tasks, getTasks }
+}
 
 const DoneTasks = () => {
+  const { tasks, getTasks } = useDoneTasks()
+
+  useEffect(() => {
+    getTasks();
+  }, [])
+  
   return (
     <ul>
-      <li className="done-task">タスク1</li>
-      <li className="done-task">タスク2</li>
+      {tasks.map(task => {
+        return <li className="done-task" key={ task.id }>{ task.name }</li>
+      })}
     </ul>
   )
 }
